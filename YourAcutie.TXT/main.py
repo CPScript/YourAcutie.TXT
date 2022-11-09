5be5da5036cf4d40f8753397e8df1a8b8934b4e1
Chai-Programming <83523587+CPScript@users.noreply.github.com>
2022-11-09 10:35:46 -0500

    Create main.py

---
+++ YourAcutie.TXT/main.py
@@ -0,0 +1,14 @@
+import time
+time.sleep(5)
+print("Your So Cute")
+time.sleep(2)
+print("Loading...")
+time.sleep(5)
+print(" ")
+print(" ")
+print("Done")
+print("<3")
+from subprocess import call
+call(["python", "YourAcutie.TXT.py"])
+time.sleep(1)
+## REMENDER add another FailSafe if the Main FailSafe doesn't work.
