0bb0cad0a63fb98b6ed14a2a9087f5ebc7657450
Chai-Programming <83523587+CPScript@users.noreply.github.com>
2022-11-08 23:06:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print("LOVE U")
+import time
 time.sleep(10)
+print("LOVE U")
+time.sleep(2)
+print("Loading...")
+time.sleep("5")
+print(" ")
+print(" ")
+print("Done")
+print("<3")
 from subprocess import call
 call(["python", "YourAcutie.TXT.py"])
